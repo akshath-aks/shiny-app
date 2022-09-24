a69f1aadf49e790811cbdee456674a184c440449
Yaning2022 <wyn198877@gmail.com>
2022-09-24 14:15:10 +0200

    changed the code

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(devtools)
 devtools::install_github("akshath-aks/Valmyndigheten")
 library(Valmyndigheten)
+result_combined<-get_combined_data()
 
 
 ui<-fluidPage(
@@ -10,18 +11,6 @@ ui<-fluidPage(
   titlePanel("Election results 2022"),
   sidebarLayout(
         sidebarPanel(
-             selectInput("party",
-                         label="Choose a party to display",
-                         choices=c("Moderaterna",
-                                   "Centerpartiet",
-                                   "Liberalerna (tidigare Folkpartiet)",
-                                   "Kristdemokraterna",
-                                   "Arbetarepartiet-Socialdemokraterna",
-                                   "Vänsterpartiet",
-                                   "Miljöpartiet de gröna",
-                                   "Sverigedemokraterna",
-                                   "Övriga anmälda partier"),
-                         selected="Moderaterna"),
              selectInput("type",
                          label="Choose a stastistic type to display data for each political party",
                          choices=c("voices 2022",
@@ -47,17 +36,12 @@ ui<-fluidPage(
                     ),
                                   
         mainPanel(
-           textOutput("selected_result"),
            plotOutput("distplot")
                   )
   )
 )
 
 server<-function(input,output){
-  output$selected_result<- renderText({ 
-    party_data<-result_combined[which(result_combined$parties==input$party),"voices 2022"]
-    paste("You have selected this",input$party,"party.",input$party,"received",party_data,"votes this year.")
-  })
   output$distplot<-renderPlot({
    ggplot(data=result_combined,aes(x=parties,y=get(input$type)))+
       geom_bar(stat='identity',fill="blue",color="black")+
