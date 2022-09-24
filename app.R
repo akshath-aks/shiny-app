342eabc3d383d1bac40ee59049dcea5d9d3c29c7
Yaning2022 <wyn198877@gmail.com>
2022-09-24 19:04:38 +0200

    changed the column names

--- app.R
+++ app.R
@@ -14,23 +14,23 @@ ui<-fluidPage(
              selectInput("type",
                          label="Choose a stastistic type to display data for each political party",
                          choices=c("voices 2022",
-                                   "shares 2022",
+                                   "shares 2022(%)",
                                    "Diff voices",
-                                   "Diff shares",
+                                   "Diff shares(%)",
                                    "voices 2018",
-                                   "shares 2018",
+                                   "shares 2018(%)",
                                    "mandate 2022",
                                    "diff mandate",
                                    "mandate 2018",
-                                   "voices 2022(a)",
-                                   "shares 2022(a)",
-                                   "Diff voices(a)",
-                                   "Diff shares(a)",
-                                   "voices 2018(a)",
-                                   "shares 2018(a)",
-                                   "mandate 2022(a)",
-                                   "diff mandate(a)",
-                                   "mandate 2018(a)"
+                                   "voices 2022 including assembly districts",
+                                   "shares 2022 including assembly districts(%)",
+                                   "Diff voices including assembly districts",
+                                   "Diff shares including assembly districts(%)",
+                                   "voices 2018 including assembly districts",
+                                   "shares 2018 including assembly districts(%)",
+                                   "mandate 2022 including assembly districts",
+                                   "Diff mandate including assembly districts",
+                                   "mandate 2018 including assembly districts"
                                    ),
                          selected="voices 2022")
                     ),
@@ -45,8 +45,7 @@ server<-function(input,output){
   output$distplot<-renderPlot({
    ggplot(data=result_combined,aes(x=parties,y=get(input$type)))+
       geom_bar(stat='identity',fill="blue",color="black")+
-      ylab(input$type)+
-      labs(title=input$type, caption="column names contain (a) include assembly districts") +
+      labs(title=input$type,y="count") +
       theme(axis.text.x = element_text(angle=45, vjust=1., hjust=1)
             ,plot.title = element_text(hjust = 0.5))
   })
