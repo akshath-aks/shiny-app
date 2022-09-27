ad0b1b27628d2f719569dd53472bdc2c81b56fed
Yaning2022 <wyn198877@gmail.com>
2022-09-27 21:15:02 +0200

    added reactive function

--- app.R
+++ app.R
@@ -42,10 +42,11 @@ ui<-fluidPage(
 )
 
 server<-function(input,output){
+  type<-reactive({input$type})
   output$distplot<-renderPlot({
-   ggplot(data=result_combined,aes(x=parties,y=get(input$type)))+
+   ggplot(data=result_combined,aes(x=parties,y=get(type())))+
       geom_bar(stat='identity',fill="blue",color="black")+
-      labs(title=input$type,x='Parties',y="Count") +
+      labs(title=type(),x='Parties',y="Count") +
       theme(axis.text.x = element_text(angle=45, vjust=1., hjust=1, face = 'bold',size=10)
             ,plot.title = element_text(hjust = 0.5, face = 'bold',size=25),
             axis.title = element_text(face='bold', size = 20))
